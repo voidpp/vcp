893eb526a3412bbaddff90b7414848cdbdfe3946
Lajos Santa <santa.lajos@coldline.hu>
2015-11-22 12:33:44 +0100

    Release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import subprocess
 
 setup(
     name = "vcp",
-    version = "1.1.0",
+    version = "1.3.0",
     description = "Version Control Projects - organize repositories",
     author = 'Lajos Santa',
     author_email = 'santa.lajos@coldline.hu',
