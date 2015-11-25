65f1520d5c9f82eb9c950ad9b5ea302ef1b91f38
Lajos Santa <santa@deverto.com>
2015-11-25 10:01:59 +0100

    Project action commands prints out the repo path too in the header

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import subprocess
 
 setup(
     name = "vcp",
-    version = "1.3.0",
+    version = "1.4.0",
     description = "Version Control Projects - organize repositories",
     author = 'Lajos Santa',
     author_email = 'santa.lajos@coldline.hu',
