46cff7c25ae5c848b1f07cf41d3c5739d743e257
Lajos Santa <santa@deverto.com>
2015-12-15 09:50:37 +0100

    Manager commands (project, repo) failed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import subprocess
 
 setup(
     name = "vcp",
-    version = "1.5.0",
+    version = "1.5.1",
     description = "Version Control Projects - organize repositories",
     author = 'Lajos Santa',
     author_email = 'santa.lajos@coldline.hu',
